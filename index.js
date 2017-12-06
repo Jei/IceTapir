7522a7f6304a0956b8902d8601aa543f746395c4
Jei <j.jonus.j@gmail.com>
2017-12-06 18:00:03 +0100

    Added Icecast and Telegram

---
+++ index.js
@@ -0,0 +1,26 @@
+const config = require('./config.json');
+const IODriver = require('./io/telegram');
+const AudioServer = require('./lib/icecast');
+const _ = require('underscore');
+
+// Console log helper
+[
+[ 'warn',  '\x1b[35m' ],
+[ 'error', '\x1b[31m' ],
+[ 'info',   '\x1b[2m' ],
+[ 'debug',   '\x1b[30m' ],
+// Custom methods
+[ 'io',   '\x1b[35m' ],
+[ 'user',   '\x1b[35m' ],
+[ 'ai',   '\x1b[35m' ],
+].forEach(function(pair) {
+  var method = pair[0], reset = '\x1b[0m', color = '\x1b[36m' + pair[1];
+  var func = console[method] || console.log;
+  console[method] = function() {
+    func.apply(console, [ color + '[' + method.toUpperCase() + ']' ].concat(_.toArray(arguments)).concat(reset) );
+  };
+});
+
+// Initialize 
+global.Telegram = new IODriver(config.telegram);
+global.Icecast = new AudioServer(config.icecast);
